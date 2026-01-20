080e8cb446c34ccd043d4a44795a25ba28589488
sarinsanyal <sarinsanyal2005@gmail.com>
2026-01-21 00:27:29 +0530

    Image added to Landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 text-4xl dark:bg-black">
 			Welcome to DoubleSlash 4.0
+			<Image src="/imgs/Devfolio_Logo-Colored.svg" height={100} width={100} alt="DEVFOLIO LOGO" />
 		</div>
 	);
 }
