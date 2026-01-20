69637ad757cafa49420da4d07794638ae0348b24
sarinsanyal <sarinsanyal2005@gmail.com>
2026-01-21 00:31:29 +0530

    added button as well to landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import DevfolioButton from "$/components/devfolio";
 
 export default function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 text-4xl dark:bg-black">
 			Welcome to DoubleSlash 4.0
 			<Image src="/imgs/Devfolio_Logo-Colored.svg" height={100} width={100} alt="DEVFOLIO LOGO" />
+			<DevfolioButton />
 		</div>
 	);
 }
